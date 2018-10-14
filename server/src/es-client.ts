359130fed3bfed1ba9fef96ef03ccaebddab040e
Ben Awad <benawad97@gmail.com>
2018-10-13 20:25:44 -0500

    retry with ping

--- server/src/es-client.ts
+++ server/src/es-client.ts
@@ -1,12 +1,27 @@
 import elasticsearch from "elasticsearch";
 
+let tries = 3;
+
+const sleep = (x: number) => new Promise(res => setTimeout(() => res(), x));
+
 export const getEsClient = async () => {
-  const client = new elasticsearch.Client({
-    host: process.env.NODE_ENV === "production" ? "es:9200" : "localhost:9200",
-    log: process.env.NODE_ENV === "production" ? undefined : "trace"
-  });
-  // 1 min
-  return client.ping({ requestTimeout: 60000 });
+  while (tries) {
+    try {
+      const client = new elasticsearch.Client({
+        host:
+          process.env.NODE_ENV === "production" ? "es:9200" : "localhost:9200",
+        log: process.env.NODE_ENV === "production" ? undefined : "trace"
+      });
+      await client.ping({});
+      return client;
+    } catch (err) {}
+    console.log(`could not connect to es ${tries} tries left`);
+    tries -= 1;
+    // wait 30 seconds
+    await sleep(30);
+  }
+
+  throw new Error("es timeout");
 };
 
 export const INDEX_NAME = "yt";
