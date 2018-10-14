5a2309de124183417266ae8fa25cd9c43c75bb6c
Ben Awad <benawad97@gmail.com>
2018-10-13 19:53:41 -0500

    increase number of retries

--- server/src/es-client.ts
+++ server/src/es-client.ts
@@ -1,8 +1,9 @@
 import elasticsearch from "elasticsearch";
 
 export const esClient = new elasticsearch.Client({
-  host: process.env.NODE_ENV ? "es:9200" : "localhost:9200",
-  log: "trace"
+  host: process.env.NODE_ENV === "production" ? "es:9200" : "localhost:9200",
+  log: process.env.NODE_ENV === "production" ? undefined : "trace",
+  maxRetries: 10
 });
 
 export const INDEX_NAME = "yt";
